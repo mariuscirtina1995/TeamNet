10125f454cb97486373c20434cf0a72d179364dc
marius.cirtina <mariuscirtina@gmail.com>
2017-07-05 17:42:32 +0300

    new_commit

---
+++ day6/ConsoleApp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            s = Console.ReadLine();
+            while (s != "exit")
+            {
+                Console.WriteLine(s);
+                s = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        
+        }
+    }
+}
